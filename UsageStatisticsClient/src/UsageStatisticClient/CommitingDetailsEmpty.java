73397dbc9957dbf604531fac64386aaf41aa1e72
Cardi <Cardi@Cardi-PC>
2011-09-07 21:52:59 +0200

    forget .classpath files

---
+++ UsageStatisticsClient/src/UsageStatisticClient/CommitingDetailsEmpty.java
@@ -0,0 +1,29 @@
+package UsageStatisticClient;
+
+class CommitingDetailsEmpty implements CommitingDetailsInterface {
+
+	@Override
+	public void commitingStart() {
+	}
+
+	@Override
+	public void commitingFinishedSuccesful() {
+	}
+
+	@Override
+	public void commitingFailureWithError(String error) {
+	}
+
+	@Override
+	public void step() {
+	}
+
+	@Override
+	public void setInfo(String info) {
+	}
+
+	@Override
+	public void setLogsAmount(int amount) {
+	}
+
+}
