d371dc447d8fc0fa8ecb460febd94279645265c8
Vi <Vi@Vi-PC.wcss.wroc.pl>
2011-10-12 20:02:38 +0200

    Aplikacja testowa

---
+++ UsageStatisticsClient/test/UsageStatisticClient/UsageStatisticTest.java
@@ -0,0 +1,79 @@
+package UsageStatisticClient;
+
+import java.sql.SQLException;
+import java.util.Calendar;
+import junitx.util.PrivateAccessor;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UsageStatisticTest {
+	
+	/*@Test
+	public void TestPost() throws URISyntaxException
+	{
+RestTemplate restTemplate = new RestTemplate();
+LogInformation log = new LogInformation();
+URI uri  =new URI("http://localhost:8080/UsageStatisticsServer/post");
+restTemplate.postForObject(uri, log, String.class);
+	}*/
+		
+	
+	/*@Test
+	public void methodUsed() throws SQLException, NoSuchFieldException
+	{
+		UsageStatistic instance = UsageStatistic.getInstance("aplikacja", null);
+		Assert.assertTrue(instance.used("funkcjonalnosc", "parametry"));
+		TestUtils.removeAllLogsFromDao(instance);
+	}*/
+	
+	/*@Test
+	public void methodCommit() throws NoSuchFieldException, SQLException, InterruptedException
+	{
+		UsageStatistic instance = UsageStatistic.getInstance("aplikacja", null);
+		for (int i=0;i<100;i++)
+		{
+			//System.out.println("ITERACJA BEFORE"+i);
+			instance.used("funkcjonalnosc", "parametry");
+			//System.out.println("ITERACJA AFTER"+i);
+		}
+		//System.out.println("COMMIT BEFORE");
+		
+		instance.setCommittingDetails(new CommitingDetailsTestImp(1));
+		instance.commit();
+		//System.out.println("COMMIT AFTER");
+		
+		
+	}*/
+		
+	@Test
+	public void methodCommit() throws NoSuchFieldException, SQLException
+	{
+		commitLogs(99);  //TODO DZIA£A
+		//commitLogs(100); //TODO NIE DZIALA!
+		
+	}
+	
+	private void commitLogs(int amountRecord) throws NoSuchFieldException, SQLException
+	{	
+		UsageStatistic instance = UsageStatistic.getInstance("aplikacja", null);
+		
+		System.out.println("INSTANCE");
+		instance.setCommittingDetails(new CommitingDetailsTestImp(amountRecord)); //przypisz na nowo za kazdym razem implementacje testujaca z podana iloscia
+		System.out.println("setCommiting");
+		TestUtils.removeAllLogsFromDao(instance);
+		System.out.println("remove");
+		TestUtils.addSomeLogsToDao(instance, amountRecord);
+		System.out.println("addSomeLogs");
+		instance.commit(); //<-- tutaj jest test ilosci oraz tego czy nie wyrzuca bledu
+		System.out.println("commit");
+		TestUtils.removeAllLogsFromDao(instance);
+		System.out.println("removeAllLogs2");
+	}
+	
+		
+	
+	
+}
+	
+	
