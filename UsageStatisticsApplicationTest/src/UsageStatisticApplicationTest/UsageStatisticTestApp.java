5af9e1d78c744535155ad34bcb50ca1a25c33f8f
Cardi <Cardi@Cardi-PC>
2011-10-14 17:14:11 +0200

    TestApp Moved

---
+++ UsageStatisticsApplicationTest/src/UsageStatisticApplicationTest/UsageStatisticTestApp.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UsageStatisticApplicationTest;
+
+import UsageStatisticClient.UsageStatistic;
+
+/**
+ *
+ * @author Vi
+ */
+public class UsageStatisticTestApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        new MainWindow().setVisible(true);
+    }
+}
