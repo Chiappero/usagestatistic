9d4dc533c3477b4aba4fc7ac647af2f87fccfcbe
krzysiek <krzysiek@DELLD820.wcss.wroc.pl>
2011-10-19 19:33:58 +0200

    Dodane AT41 i AT42, jeszcze nie sprawdzone czy przechodz? dodany ConfigGenerator oraz FileCopy

--- UsageStatisticsClient/test/UsageStatisticClient/TestUtils.java
+++ UsageStatisticsClient/test/UsageStatisticClient/TestUtils.java
@@ -1,8 +1,11 @@
 package UsageStatisticClient;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.GregorianCalendar;
+
+import UsageStatisticClientConfigGenerator.ConfigGenerator;
 
 import junitx.util.PrivateAccessor;
 
@@ -63,5 +66,11 @@ public static void makeConnectionNull(DaoTemporaryDatabaseH2 dao)
 {
 	dao.conn=null;
 }
+
+public static void createExampleConfigFile() throws IOException
+{
+	ConfigGenerator.createConfigFile("client-config.cfg", "http://localhost:8080/UsageStatisticsServer","matuszek","password");
+}
+
 
 }
