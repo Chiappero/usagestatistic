7f35d337fe5868c613b7b03cd9950b548fba24ce
Vi <Vi@Vi-PC>
2011-10-19 22:12:44 +0200

    commit in thread

---
+++ UsageStatisticsClient/test/UsageStatisticClient/TestUtils.java
@@ -0,0 +1,67 @@
+package UsageStatisticClient;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.GregorianCalendar;
+
+import junitx.util.PrivateAccessor;
+
+public class TestUtils
+{
+public static void removeAllLogsFromDao(UsageStatistic instance) throws NoSuchFieldException, SQLException
+{
+	
+	DaoTemporaryDatabaseH2 dao = (DaoTemporaryDatabaseH2) PrivateAccessor.getField(instance, "dao");
+	while (!dao.isEmpty())
+	{
+		dao.clearFirstLog();
+	}
+}
+
+
+public static int getLogsAmmount(UsageStatistic instance) throws NoSuchFieldException, SQLException
+{
+	
+	DaoTemporaryDatabaseH2 dao = (DaoTemporaryDatabaseH2) PrivateAccessor.getField(instance, "dao");
+	return dao.getLogsAmount();
+}
+
+public static void addSomeLogsToDao(UsageStatistic instance, int amountRecord)
+{	
+	for (int i=0;i<amountRecord;i++)
+	{
+	instance.used("funkcjonalnosc", "i="+i); 
+	}
+}
+
+public static DaoTemporaryDatabaseH2 getLocalDao(UsageStatistic instance) throws NoSuchFieldException
+{
+	return (DaoTemporaryDatabaseH2) PrivateAccessor.getField(instance, "dao");
+}
+
+
+
+public static void corruptFile(UsageStatistic instance) throws NoSuchFieldException, SQLException
+{
+	DaoTemporaryDatabaseH2 dao = (DaoTemporaryDatabaseH2) PrivateAccessor.getField(instance, "dao");
+	dropTable(dao);
+}
+
+public static LogInformation getExampleLog()
+{
+	return new LogInformation(new GregorianCalendar().getTime(), "test", "test", "test", "test");
+}
+
+public static void dropTable(DaoTemporaryDatabaseH2 dao) throws NoSuchFieldException, SQLException
+{
+	String sql="DROP TABLE Log";
+	Connection conn=(Connection) PrivateAccessor.getField(dao, "conn");
+	conn.createStatement().execute(sql);
+}
+
+public static void makeConnectionNull(DaoTemporaryDatabaseH2 dao)
+{
+	dao.conn=null;
+}
+
+}
