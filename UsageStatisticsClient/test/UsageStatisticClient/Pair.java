bae25cf6e1cb2a6d79179a7eec77f54ce48f7b45
krzysiek <krzysiek@DELLD820.wcss.wroc.pl>
2011-10-26 18:38:53 +0200

    Dodane klasy Pair

---
+++ UsageStatisticsClient/test/UsageStatisticClient/Pair.java
@@ -0,0 +1,24 @@
+package UsageStatisticClient;
+
+public class Pair<Lewy,Prawy>
+{
+Lewy lewy;
+Prawy prawy;
+public Lewy getLewy()
+{
+	return lewy;
+}
+public void setLewy(Lewy lewy)
+{
+	this.lewy = lewy;
+}
+public Prawy getPrawy()
+{
+	return prawy;
+}
+public void setPrawy(Prawy prawy)
+{
+	this.prawy = prawy;
+}
+
+}
