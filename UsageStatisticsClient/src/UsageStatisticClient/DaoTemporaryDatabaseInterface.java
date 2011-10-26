ecfe11fcceab40ee1c49ce5350cae053f4557c66
Chicken chuj <Chicken chuj@PawelPC.wcss.wroc.pl>
2011-10-26 20:09:55 +0200

    PN- sortowanie log?w

---
+++ UsageStatisticsClient/src/UsageStatisticClient/DaoTemporaryDatabaseInterface.java
@@ -0,0 +1,16 @@
+package UsageStatisticClient;
+
+import java.sql.SQLException;
+
+interface DaoTemporaryDatabaseInterface {
+
+boolean saveLog(LogInformation log);
+void clearFirstLog() throws SQLException; 
+LogInformation getFirstLog() throws SQLException;
+boolean isEmpty() throws SQLException;
+int getLogsAmount() throws SQLException;
+void openDatabase();
+void closeDatabase();
+void resetDatabase();
+
+}
