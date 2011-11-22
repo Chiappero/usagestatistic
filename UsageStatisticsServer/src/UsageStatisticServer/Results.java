5de31969d80891b8699e32e97e37123176535dfd
Vi <Vi@Vi-PC>
2011-11-22 11:58:30 +0100

    fix enkapsulacji bo seleniumowe testy nie dzialaly

--- UsageStatisticsServer/src/UsageStatisticServer/Results.java
+++ UsageStatisticsServer/src/UsageStatisticServer/Results.java
@@ -4,8 +4,7 @@ import java.util.ArrayList;
 import java.util.Date;
 
 
-
-class Results
+public class Results
 {
 private String tool;
 private String[] functionalities;
@@ -14,48 +13,48 @@ private String dateFrom;
 private String dateTill;
 private boolean param;
 
-boolean isParam() {
+public boolean isParam() {
 	return param;
 }
-/*private void setParam(boolean param) {
+public void setParam(boolean param) {
 	this.param = param;
-}*/
-String getDateTill() {
+}
+public String getDateTill() {
 	return dateTill;
 }
-/*private void setDateTill(String dateTill) {
+public void setDateTill(String dateTill) {
 	this.dateTill = dateTill;
-}*/
-String getDateFrom() {
+}
+public String getDateFrom() {
 	return dateFrom;
 }
-/*private void setDateFrom(String dateFrom) {
+public void setDateFrom(String dateFrom) {
 	this.dateFrom = dateFrom;
-}*/
-String getTool()
+}
+public String getTool()
 {
 	return tool;
 }
-/*private void setTool(String tool)
+public void setTool(String tool)
 {
 	this.tool = tool;
-}*/
-String[] getFunctionalities()
+}
+public String[] getFunctionalities()
 {
 	return functionalities;
 }
-/*private void setFunctionalities(String[] functionalities)
+public void setFunctionalities(String[] functionalities)
 {
 	this.functionalities = functionalities;
-}*/
-String[] getUsers()
+}
+public String[] getUsers()
 {
 	return users;
 }
-/*private void setUsers(String[] users)
+public void setUsers(String[] users)
 {
 	this.users = users;
-}*/
+}
 
 
 
